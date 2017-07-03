e22a8d52e69bf43d8939befbba467ffdbdcdd692
AKD47 <van83horn@gmail.com>
2017-07-03 13:02:15 +0300

    apartments slider

--- app/js/script.js
+++ app/js/script.js
@@ -1,29 +1,30 @@
 $(document).ready(function () {
-    $('.owl-carousel2').owlCarousel({
-        loop: true,
-        margin: 10,
-        dots: true,
-        navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-        responsive:{
-            0:{
-                items: 1,
-                nav: true,
-                dots: true
-            },
-            600:{
-                items: 1,
-                nav: false,
-                autoplay: false,
-                dots: true
-            },
-            1000:{
-                items: 1,
-                nav: true,
-                autoplay: true,
-                dots: true
+    
+    /*apartments slider*/    
+    $('#apartments-thumbnail').flexslider({
+        animation: 'slide',
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        itemWidth: 220,
+        itemMargin: 15,
+        asNavFor: '#apartments-slider',
+        responsive: {
+            1024: {
+                itemWidth: 250
             }
         }
+
     });
+    $('#apartments-slider').flexslider({
+        animation: 'slide',
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        sync: '#apartments-thumbnail'
+    });
+
+    /*close apartments slider*/
 
 
     $('#reviews-slider').owlCarousel({
@@ -65,27 +66,25 @@ $(document).ready(function () {
     });
 
     /*map*/
-
-        var map = new Map();
-        map.init({
-            selector:'#contacts-map',
-            center: $('.contacts__data p').text(),
-            zoom: 17,
-            placemarks: [
-                {
-                    address: $('.contacts__data p').text(),
-                    options: [
-                        {key:'draggable',value:true}
-                    ],
-                    properties: [
-                        /*{key:'hintContent',value:''},
-                         {key:'balloonContentHeader', value:"Предприятия Самары"},
-                         {key:'balloonContentBody', value:"<h1>Название</h1>"}*/
-                    ]
-                }
-            ]
-        });
-        /*close map*/
-
+    /*var map = new Map();
+    map.init({
+        selector: '#contacts-map',
+        center: $('.contacts__data p').text(),
+        zoom: 17,
+        placemarks: [
+            {
+                address: $('.contacts__data p').text(),
+                options: [
+                    {key: 'draggable', value: true}
+                ],
+                properties: [
+                    /!*{key:'hintContent',value:''},
+                     {key:'balloonContentHeader', value:"Предприятия Самары"},
+                     {key:'balloonContentBody', value:"<h1>Название</h1>"}*!/
+                ]
+            }
+        ]
+    });*/
+    /*close map*/
 
 });
